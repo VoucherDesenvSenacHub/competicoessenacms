af4c3429b773d5b0a5f5bd7df09e4f06db09815d
DEVR01001 <rafael.dev01001@gmail.com>
2025-04-23 16:27:10 -0400

    resposividade index

--- Evento/js/card.js
+++ Evento/js/card.js
@@ -6,4 +6,34 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+
+    breakpoints: {
+
+      // when window width is >= 640px
+      1621: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+
+      800: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+
+      485: {
+        slidesPerView: 2,
+        spaceBetween: 50
+      },
+
+      300: {
+        slidesPerView: 1,
+        spaceBetween:0
+      }
+    
+    }
   });
---
+++ Evento/js/mobile.js
@@ -0,0 +1,13 @@
+
+
+
+let opneBtn = document.getElementById('icon_open')
+
+let ContainerOpne = document.getElementById('conatiner_open')
+
+
+
+
+opneBtn.addEventListener('click', ()=>{
+    ContainerOpne.classList.toggle('active_nav')
+})
