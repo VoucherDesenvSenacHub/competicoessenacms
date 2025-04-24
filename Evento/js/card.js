631f4012eaece26959097502a2a68eaf31af84e0
Fábrica De Software <voucherdesenv.hub@ms.senac.br>
2025-04-24 14:09:48 -0300

    animação card loop

--- Evento/js/card.js
+++ Evento/js/card.js
@@ -2,6 +2,11 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: -400,
     grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
